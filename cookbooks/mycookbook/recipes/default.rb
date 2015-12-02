56b8e6d189b7bf3647f0ad04e2c61a63875a6f9a
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2015-12-02 11:37:39 +0530

    first commit

---
+++ cookbooks/mycookbook/recipes/default.rb
@@ -0,0 +1,6 @@
+file "/tmp/default-file" do
+  content "It's file created by Chef Recipe"
+  owner "root"
+  group "root"
+  mode 00600
+end
---
+++ cookbooks/mycookbook/recipes/first.rb
@@ -0,0 +1,6 @@
+file "/tmp/default-file-new" do
+  content "It's another file created by Chef Recipe"
+  owner "root"
+  group "root"
+  mode 00600
+end
